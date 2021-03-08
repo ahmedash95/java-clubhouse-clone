c717792ab3d4249c11f0581397cf66ae4ccf1969
Ahmed Ashraf <ahmed29329@gmail.com>
2021-03-08 20:00:39 +0100

    password encoder

--- src/main/java/com/egy/clubhouse_clone/ApplicationSecurity.java
+++ src/main/java/com/egy/clubhouse_clone/ApplicationSecurity.java
@@ -1,13 +1,23 @@
 package com.egy.clubhouse_clone;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
